73ce1746a4b3db0684dc4940a218a17ee9129fe8
zuosenyu <15660616996@163.com>
2024-01-27 10:30:11 +0800

    分页入参新增非空校验

--- poetize-common/src/main/java/com/ld/poetize/utils/PageUtil.java
+++ poetize-common/src/main/java/com/ld/poetize/utils/PageUtil.java
@@ -1,6 +1,7 @@
 package com.ld.poetize.utils;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -11,8 +12,10 @@ import lombok.Data;
 public class PageUtil {
 
     @Schema(description = "当前页码")
+    @NotNull(message = "当前页码不能为空")
     private Long current;
 
     @Schema(description = "每页显示数")
+    @NotNull(message = "每页显示数不能为空")
     private Long size;
 }
