c0d5b9e797b25a10f6f80cdc25a4f42304b1b9af
zuosenyu <15660616996@163.com>
2024-02-01 21:21:00 +0800

    增加@Permitall白名单注解配置

--- poetize-start/src/main/java/com/ld/poetize/config/AuthorizationClientConfig.java
+++ poetize-start/src/main/java/com/ld/poetize/config/AuthorizationClientConfig.java
@@ -8,9 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @Author zuosy
  * @Date 2023/12/10 19:49
@@ -30,21 +27,9 @@ public class AuthorizationClientConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
-
-        //不校验的路径
-        Set<String> set = new HashSet<>();
-        String[] ignoreUrls = set.toArray(new String[0]);
-
-        http.authorizeHttpRequests(authorize ->
-                authorize.requestMatchers(ignoreUrls).permitAll()
-                        //其他请求，需要认证
-                        .anyRequest().authenticated()
-        );
-
         http.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(Customizer.withDefaults())
         );
         return http.build();
-
     }
 }
--- poetize-start/src/main/java/com/ld/poetize/config/DefaultSecurityConfig.java
+++ poetize-start/src/main/java/com/ld/poetize/config/DefaultSecurityConfig.java
@@ -2,6 +2,7 @@ package com.ld.poetize.config;
 
 import com.ld.poetize.handler.AuthenticationEntryPointHandler;
 import com.ld.poetize.handler.MyAccessDeniedHandler;
+import jakarta.annotation.security.PermitAll;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 认证中心默认配置
@@ -21,6 +27,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class DefaultSecurityConfig {
 
+    private final RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    public DefaultSecurityConfig(RequestMappingHandlerMapping requestMappingHandlerMapping) {
+        this.requestMappingHandlerMapping = requestMappingHandlerMapping;
+    }
+
     /**
      * Spring Security 过滤链配置（此处是纯Spring Security相关配置）
      */
@@ -28,13 +40,16 @@ public class DefaultSecurityConfig {
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
             throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(
-                                "/login",
-                                "/v3/api-docs/**",
-                                "/doc.html",
-                                "/favicon.ico",
-                                "/webjars/**"
-                        ).permitAll()
+        //白名单及被@Permitall注解修饰的接口地址
+        Set<String> ignoreUrl = getUrlsFormPermitAllAnnotation();
+        ignoreUrl.add("/login");
+        ignoreUrl.add("/v3/api-docs/**");
+        ignoreUrl.add("/doc.html");
+        ignoreUrl.add("/favicon.ico");
+        ignoreUrl.add("/webjars/**");
+        String[] array = ignoreUrl.toArray(new String[0]);
+
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(array).permitAll()
                         .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin.loginPage("/login"));
         http.oauth2ResourceServer((resourceServer) -> resourceServer
@@ -46,5 +61,21 @@ public class DefaultSecurityConfig {
         http.cors(AbstractHttpConfigurer::disable);
         http.headers(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    /**
+     * 获取被PermitAll注解修饰的url地址
+     */
+    private Set<String> getUrlsFormPermitAllAnnotation() {
+        Set<String> result = new HashSet<>();
+        requestMappingHandlerMapping.getHandlerMethods().forEach((key, method) -> {
+            if (method.hasMethodAnnotation(PermitAll.class)) {
+                Set<String> urls = key.getPatternValues();
+                if (!CollectionUtils.isEmpty(urls)) {
+                    result.addAll(urls);
+                }
+            }
+        });
+        return result;
     }
 }
